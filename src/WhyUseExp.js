9b5aac28204f8338182d10c698da36c26ae177db
mohammad <mgharebaghi82@gmail.com>
2023-10-28 16:17:17 +0330

    change why should we use comp

--- src/WhyUseExp.js
+++ src/WhyUseExp.js
@@ -22,7 +22,7 @@ function WhyUseExp() {
         minHeight: "600px",
         display: "grid",
         alignContent: "center",
-        backgroundColor:"#FFBE88"
+        backgroundColor: "#FFBE88",
       }}
       data-aos="fade-up"
     >
@@ -41,16 +41,15 @@ function WhyUseExp() {
               <Typography style={{ fontWeight: "bold" }} variant="h3">
                 Why should we use Centichain?
               </Typography>
-              <Typography variant="h5" style={{lineHeight:"1.7"}}>
-                Centichain is a decentralized digital asset which you can use as
-                money or store of value. Like other decentralized digital
-                assets, Centichain does not need a reliable third person
-                (middlemen like the banks). However, it technically presents
-                dissimilitude to others, something like a consensus mechanism,
-                and so can be more effective for network participants with
-                regard to energy consumption and cost. In fact, Centichain is a
-                blockchain network for a safe value saving and transferring,
-                both energy-wise and financial-wise.
+              <Typography variant="h5" style={{ lineHeight: "1.7" }}>
+                If you think maintaining mining machines is hard, participating
+                in mining pools is unsafe, paying to be in a blockchain network,
+                named stake, is unfair and costly or if you intend to set your
+                computer in a blockchain bed decentralizedly as a member in a
+                digital-asset keeping network, or even if you believe it is
+                important to create a new independent and self-generated
+                non-inflating value without any complicated structures and
+                devices, Centichain could be gainful.
               </Typography>
             </ThemeProvider>
           </Col>
