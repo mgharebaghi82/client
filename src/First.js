1d3a1bf5298b78da5414d2923ff8c00036ee6a57
mohammad <mgharebaghi82@gmail.com>
2023-11-18 22:50:54 +0330

    change port3

--- src/First.js
+++ src/First.js
@@ -18,7 +18,7 @@ function First() {
   const [mainData, setMainData] = useState([]);
   const [dataLoaded, setDataLoade] = useState(false);
   useEffect(() => {
-    axios.get("https://centichain.org").then((res) => {
+    axios.get("https://centichain.org:3002").then((res) => {
       mainData.push(res.data);
       setMainData(mainData);
       setDataLoade(true);
